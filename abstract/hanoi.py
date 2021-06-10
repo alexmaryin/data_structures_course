48d836ebaeecee7bbdac36ea97c09f56ea3a3cc5
alexmaryin <java73@yandex.ru>
2021-06-10 18:38:18 +0400

    Hanoi tower moving of discs implemented

---
+++ abstract/hanoi.py
@@ -0,0 +1,26 @@
+from stack.stack import Stack
+
+
+def solve(left: Stack[int], right: Stack[int], temp: Stack[int], n):
+    if n == 1:
+        right.push(left.pop())
+    else:
+        solve(left, temp, right, n - 1)
+        solve(left, right, temp, 1)
+        solve(temp, right, left, n -1)
+
+
+def main():
+    discs_count = 4
+    tower1: Stack[int] = Stack()
+    tower2: Stack[int] = Stack()
+    tower3: Stack[int] = Stack()
+    for i in range(discs_count, 0, -1):
+        tower1.push(i)
+    print(tower1, tower2, tower3)
+    solve(tower1, tower3, tower2, discs_count)
+    print(tower1, tower2, tower3)
+
+
+if __name__ == "__main__":
+    main()
