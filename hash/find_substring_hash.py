2e11aed1fe3c3e249db2d01de6236b2d793bda78
alexmaryin <java73@yandex.ru>
2021-07-07 18:34:31 +0400

    Search substring with hash corrected!

--- hash/find_substring_hash.py
+++ hash/find_substring_hash.py
@@ -1,40 +1,43 @@
-class HashSearch:
-    def __init__(self):
-        self.P = 1000000007
-        self.X = 263
-        self.squads = {0: 1}
-        self.memo = []
+from functools import reduce
 
-    def get_squad_mod(self, x):
-        if x not in self.squads:
-            self.squads[x] = self.get_squad_mod(x - 1) * self.X % self.P
-        return self.squads[x]
 
-    def get_full_hash(self, string, length=-1):
-        hash_code = 0
-        for idx, sym in enumerate(string):
-            hash_code += ord(sym) * self.get_squad_mod(idx)
-            if length > 0:
-                self.memo.append(ord(sym) * self.get_squad_mod(length - 1))
-        print(f'hash for {string} is {hash_code % self.P}')
-        return hash_code % self.P
+def check_equal(first: str, second: str):
+    for sym1, sym2 in zip(first, second):
+        if sym1 != sym2:
+            return False
+    return True
 
-    def slide_hash(self, text, left, right, previous):
-        hash_code = ord(text[left]) + previous - self.memo[len(text) - right - 1]
-        self.memo.append(ord(text[left]) * self.get_squad_mod(right - left))
-        print(f'hash for slide {text[left:right + 1]} is {hash_code}')
-        return hash_code
+
+def build_z_hash(string: str):
+    n = len(string)
+    z = [0] * n
+    left, right = 0, 0
+    for idx in range(1, n):
+        if idx <= right:
+            z[idx] = min(right - idx + 1, z[idx - 1])
+        while idx + z[idx] < n and string[z[idx]] == string[idx + z[idx]]:
+            z[idx] += 1
+        if idx + z[idx] - 1 > right:
+            left, right = right, idx + z[idx] - 1
+    return z
+
+
+def get_hash(string: str, length, pos=0, previous=None):
+    if previous:
+        h = previous - ord(string[pos - 1]) + ord(string[pos + length - 1])
+    else:
+        h = reduce(lambda acc, sym: acc + ord(sym), string[pos:pos + length], 0)
+    # print(f'Hash for {string[pos:pos + length]} is {h}')
+    return h
 
 
 if __name__ == '__main__':
     pattern, text = input(), input()
-    assert len(pattern) <= len(text)
-    left = len(text) - len(pattern)
-    search = HashSearch()
-    hash_pattern, hash_str = search.get_full_hash(pattern), search.get_full_hash(text[left:], len(pattern))
+    l = len(pattern)
+    pattern_hash, text_hash = get_hash(pattern, l), None
     res = []
-    for idx in range(left, 1, -1):
-        if hash_str == hash_pattern and text[idx:idx + len(pattern)] == pattern:
-            res.append(str(idx))
-        hash_str = search.slide_hash(text, idx - 1, idx + len(pattern) - 2, hash_str)
+    for i in range(0, len(text) - l + 1):
+        text_hash = get_hash(text, l, i, text_hash)
+        if pattern_hash == text_hash and pattern == text[i: i + l]:
+            res.append(str(i))
     print(' '.join(res))
